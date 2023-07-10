103e05457b9bab37b410f2eebb184c06db1075ff
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-07-11 07:57:37 +0900

    RuntimeConfig に Open AI の API キーを追加

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,10 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: { enabled: true },
+  alias: {
+    "@Domains/*": "../domains/*",
+  },
   css: ["~/assets/css/tailwind.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+  runtimeConfig: {
+    openAiApiKey: "",
   },
 });
