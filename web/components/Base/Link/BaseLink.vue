1a0f7076e35964d0b07c1a4ec66f6e696dcc0825
Nemoto Masaharu <57859567+harusame0616@users.noreply.github.com>
2023-07-12 20:46:56 +0900

    ベースレイアウト作成 (#6)

---
+++ web/components/Base/Link/BaseLink.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    href: string;
+    screenReaderText: string;
+  }>(),
+  {
+    screenReaderText: "",
+  }
+);
+</script>
+
+<template>
+  <NuxtLink :href="href">
+    <slot />
+    <span class="sr-only">{{ screenReaderText }}</span>
+  </NuxtLink>
+</template>
---
+++ web/components/Page/PageFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="bg-[#F59751]">
+    <LayoutBox thin center>
+      <span class="text-xs text-white font-bold"> © 2023 Harusame </span>
+    </LayoutBox>
+  </footer>
+</template>
