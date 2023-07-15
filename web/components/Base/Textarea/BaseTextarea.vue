f5ab8fae1f7388e80db589431ecec21266e59b7c
Nemoto Masaharu <57859567+harusame0616@users.noreply.github.com>
2023-07-15 09:05:42 +0900

    BaseTextarea 作成 (#13)

---
+++ web/components/Base/Textarea/BaseTextarea.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ srText?: string }>(), {
+  srText: "",
+});
+const isFocused = ref(false);
+</script>
+
+<template>
+  <LayoutBox
+    class="rounded-md border transition-all"
+    :class="{
+      'border-gray-200': !isFocused,
+      'border-secondary-light': isFocused,
+    }"
+  >
+    <span v-if="srText" class="sr-only">{{ srText }}</span>
+    <textarea
+      v-bind="$attrs"
+      class="h-full w-full focus:outline-none disabled:text-gray-400"
+      @focus="isFocused = true"
+      @blur="isFocused = false"
+    />
+  </LayoutBox>
+</template>
