1bb5746b59a8dab2d7c3365ce9bc964c8dd51055
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-07-17 08:51:12 +0900

    LayoutSpacer 作成

---
+++ web/components/Layout/Spacer/LayoutSpacer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{ large?: boolean }>(), {
+  large: false,
+});
+
+const size = computed(() => (props.large ? "h-8" : "h-4"));
+</script>
+
+<template>
+  <div :class="size"></div>
+</template>
