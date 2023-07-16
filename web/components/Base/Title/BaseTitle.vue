bb348f8369427c9082e7dc7db4de6835eb7c10f6
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-07-17 08:38:34 +0900

    BaseTitle 追加

---
+++ web/components/Base/Title/BaseTitle.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{ level: 1 | 2 | 3 | 4 }>();
+const titleTag = computed(() => `h${props.level}`);
+
+const sizes = {
+  1: "text-xl",
+  2: "text-lg",
+  3: "text-md",
+  4: "text-sm",
+};
+const size = computed(() => sizes[props.level]);
+</script>
+
+<template>
+  <component :is="titleTag" class="font-bold text-text-primary" :class="size">
+    <slot />
+  </component>
+</template>
